f6481dcb653155adf34b1914f0777c670392be77
Christiaan Bloemendaal <github@tnrd.net>
2023-10-12 20:24:23 +0200

    fix: nulling of User

--- Models/Favorite.cs
+++ Models/Favorite.cs
@@ -5,11 +5,11 @@ namespace TNRD.Zeepkist.GTR.Database.Models;
 [Table("favorites")]
 public partial class Favorite : ModelBase
 {
-    [Column("user")] public int? User { get; set; }
+    [Column("user")] public int User { get; set; }
 
     [Column("level")] public string Level { get; set; } = null!;
 
     [ForeignKey("User")]
     [InverseProperty("Favorites")]
-    public virtual User? UserNavigation { get; set; }
+    public virtual User UserNavigation { get; set; } = null!;
 }
